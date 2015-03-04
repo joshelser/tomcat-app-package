8561f4555591df893253505a06f20dcccf168df2
Josh Elser <josh.elser@gmail.com>
2015-03-04 11:40:52 -0800

    Initial (non-working) attempt

---
+++ package/scripts/tomcat.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+import sys
+from resource_management import *
+
+class Tomcat(Script):
+  def install(self, env):
+    self.install_packages(env)
+    pass
+
+  def configure(self, env):
+    import params
+    env.set_params(params)
+
+  def start(self, env):
+    import params
+    self.configure(env)
+    process_cmd = format('env JAVA_HOME={java64_home} JSVC={jsvc} {app_root}/apache-tomcat-*/bin/daemon.sh --catalina-pid ' + params.pid_file + ' --tomcat-user $USER start')
+
+    Execute(process_cmd,
+        logoutput=False,
+        wait_for_finish=False,
+        pid_file=params.pid_file,
+        poll_after = 5
+    )
+
+  def stop(self, env):
+    import params
+    self.configure(env)
+    process_cmd = format('env JAVA_HOME={java64_home} JSVC={jsvc} {app_root}/apache-tomcat-*/bin/daemon.sh --catalina-pid ' + params.pid_file + ' --tomcat-user $USER stop')
+
+    Execute(process_cmd,
+        logoutput=False,
+        wait_for_finish=False,
+        pid_file=params.pid_file,
+        poll_after = 5
+    )
+
+  def status(self, env):
+    import params
+    self.configure(env)
+    check_process_status(params.pid_file)
+
+if __name__ == "__main__":
+  Tomcat().execute()
